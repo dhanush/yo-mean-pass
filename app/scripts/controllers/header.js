7b03acaeb199f9437b642dc3915689bba436a5b2
Dhanush Gopinath <dhanush@beyondbytes.co.in>
2014-02-17 23:51:08 +0530

    Initial Files

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('mean.system').controller('HeaderController', ['$scope', 'Global', function ($scope, Global) {
+    $scope.global = Global;
+
+// TODO: Add your items here to appear in NavBar 
+    
+//    $scope.menu = [{
+//        'title': 'Articles',
+//        'link': 'articles'
+//    }, {
+//        'title': 'Create New Article',
+//        'link': 'articles/create'
+//    }];
+    
+    $scope.isCollapsed = false;
+}]);
